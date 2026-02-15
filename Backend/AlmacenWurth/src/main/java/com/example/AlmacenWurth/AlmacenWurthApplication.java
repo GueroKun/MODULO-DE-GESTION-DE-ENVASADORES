60838f6a098a88c4887273e7b3602906e403abc3
GAreeem <20223tn084@utez.edu.mx>
2026-02-15 15:26:25 -0600

    backend

---
+++ Backend/AlmacenWurth/src/main/java/com/example/AlmacenWurth/AlmacenWurthApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlmacenWurth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenWurthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenWurthApplication.class, args);
+	}
+
+}
---
+++ Backend/AlmacenWurth/src/test/java/com/example/AlmacenWurth/AlmacenWurthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlmacenWurth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenWurthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
